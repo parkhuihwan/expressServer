9a3049e3d0ade9ca4dd88f5ef405d0bbac50c5fd
HuihwanPark <the_night@naver.com>
2019-03-05 20:34:23 +0900

    promise ajax

--- webServer/texts/testSyncAsyncAjax.js
+++ webServer/texts/testSyncAsyncAjax.js
@@ -10,9 +10,6 @@
         console.log('start jquery!');
         $(document).ready(function () {
           // code ...
-          console.log('start jquery! 1. ready');
-        });
-  
         console.log('first ajax start!');
         $.ajax({                
                   type: 'POST',
@@ -32,4 +29,15 @@
             });
   
         console.log('second ajax end!');
+        });
+
+        $.ajax( {
+                  type: 'POST',
+                  url: 'http://192.168.0.103:8888/asyncPost',   
+                  success: function(){console.log('success callback func2() called!')},
+                  complete: function() {console.log('complete callback func2() called!')}, 
+        })         
+        .then(function() { console.log('done'); }, function() { console.log('fail'); })         
+        .catch(function() { console.log('always'); }); 
+
       }
